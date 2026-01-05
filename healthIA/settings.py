a40e529f4cd9eb06560477acc1e6e07c166ace54
Namania <mael.namania@gmail.com>
2026-01-05 14:23:11 +0100

    :bug: Add allowed host & csrf trusted origins

--- healthIA/settings.py
+++ healthIA/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
 
 INSTALLED_APPS = [
     'app',
