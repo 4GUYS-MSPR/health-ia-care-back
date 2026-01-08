ab91cbad85644ebdec3261abd65c5bd4c59c9eb2
Sébastien Lotten <sebastienlotten@proton.me>
2026-01-08 14:20:48 +0100

    chore: add primary key on models panel

--- app/admin/exercice.py
+++ app/admin/exercice.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from app.models.exercice import Exercice
-
 class ExerciceAdmin(admin.ModelAdmin):
-    list_display = ['image_url', 'category']
+    list_display = ['pk','image_url', 'category']
     list_filter = ['category', 'equipments' ]
 
