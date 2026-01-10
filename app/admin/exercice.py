452f2338abaabd6c79a4a4111abb386433ea83a1
Namania <mael.namania@gmail.com>
2026-01-10 16:12:01 +0100

    feat: Customize admin panel

--- app/admin/exercice.py
+++ app/admin/exercice.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
+from app.admin.body_part import BodyPartInline
+from app.admin.equipment import EquipmentInline
+
 class ExerciceAdmin(admin.ModelAdmin):
 
-    list_display = ['pk','image_url', 'category']
-    list_filter = ['category', 'equipments' ]
+    list_display = ['pk', 'category', 'image_url']
+    list_filter = ['category']
+
+    search_fields = ['pk', 'category__value', 'image_url']
+
+    fieldsets = [
+        (None, {"fields": ["image_url", "category"]}),
+    ]
+    
+    inlines = [EquipmentInline, BodyPartInline]
+    # secondary_muscles
+    # target_muscles
