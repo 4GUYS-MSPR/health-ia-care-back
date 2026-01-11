c572f6d82cb41b5bf67942742af62476ec106f72
Sébastien Lotten <sebastienlotten@proton.me>
2026-01-11 10:27:27 +0100

    feat:WIP signals

--- app/signals.py
+++ app/signals.py
@@ -1,11 +1,11 @@
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
+#from django.dispatch import receiver
+#from django.db.models.signals import post_save
+#from django.contrib.auth.models import User
 
 
-@receiver(post_save, sender=User)
-def create_user_token(sender, instance, created, **kwargs):
-    import sys
-    sys.stdout.flush()
+#@receiver(post_save, sender=User)
+#def create_user_token(sender, instance, created, **kwargs):
+    #import sys
+    #sys.stdout.flush()
 
     
