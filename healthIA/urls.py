4fcdc58ea15640a81c621f86d213a1b6962bd89e
Sébastien Lotten <sebastienlotten@proton.me>
2026-01-11 10:11:14 +0100

    feat:install django rest framework and and post_save signal

--- healthIA/urls.py
+++ healthIA/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
