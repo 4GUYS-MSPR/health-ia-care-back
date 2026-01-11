9f009c6c3a65d8c6e847f2099d890190e1df7456
Namania <mael.namania@gmail.com>
2026-01-11 11:47:49 +0100

    [20] lint

--- healthIA/urls.py
+++ healthIA/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from rest_framework.authtoken import views 
+from rest_framework.authtoken import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
     path('token/', views.obtain_auth_token),
-    path('admin/', admin.site.urls),
 ]
