0b685f3c953d86ba0fe495fbe36eb1ff8c5c85d2
Sébastien Lotten <sebastienlotten@proton.me>
2026-01-20 13:26:55 +0100

    feat[31]: apply linter

--- app/views/members.py
+++ app/views/members.py
@@ -1,5 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
 from app.models import Member
 from app.serializers.member import MemberSerializer
 
