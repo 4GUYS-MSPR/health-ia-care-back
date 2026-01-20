0da4c53d82a3a7d49411123ddd6f1b826559e824
mael.boyaval <mael.boyaval@elosi.com>
2026-01-20 14:20:11 +0100

    [28] /users/me retrieves the authenticated user's personal

--- app/views/users.py
+++ app/views/users.py
@@ -1,12 +1,21 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
 from app.serializers.user import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
 
     def get_queryset(self):
         user = self.request.user
         return User.objects.all().order_by('id') if user.is_staff else User.objects.filter(id=user.id).order_by('id')
+
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
