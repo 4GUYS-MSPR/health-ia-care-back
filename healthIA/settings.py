36a689d44a928bee66bcf98b052abe13959099de
mael.boyaval <mael.boyaval@elosi.com>
2026-01-26 10:14:52 +0100

    [49] feat: Add custom logo

--- healthIA/settings.py
+++ healthIA/settings.py
@@ -85,6 +85,7 @@ JAZZMIN_SETTINGS = {
     "site_header": "HealthIA",
     "site_brand": "HealthIA",
     "welcome_sign": "Welcome to HealthIA !",
+    "site_logo": "/logo.webp",
 }
 
 MIDDLEWARE = [
@@ -105,10 +106,11 @@ ROOT_URLCONF = 'healthIA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
