2813c9337f4a75ca823957d3bdb5a04ecb27b04a
Namania <mael.namania@gmail.com>
2026-01-31 21:43:55 +0100

    [00] fix: logs api route -> only staff

--- logs/views.py
+++ logs/views.py
@@ -1,8 +1,11 @@
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Log
 from .serializers import LogSerializer
 
 class LogViewSet(ModelViewSet):
+    permission_classes = [IsAdminUser]
+
     serializer_class = LogSerializer
     queryset = Log.objects.all()
