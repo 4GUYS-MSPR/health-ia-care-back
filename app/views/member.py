75fba76b5f4ba2dbf2faf9f12c0e73bf4c0204cf
Namania <mael.namania@gmail.com>
2026-02-10 20:10:34 +0100

    [69] HORS SUJET: remove print

--- app/views/member.py
+++ app/views/member.py
@@ -9,7 +9,6 @@ class MemberViewSet(ModelViewSet):
     serializer_class = MemberSerializer
 
     def perform_create(self, serializer):
-        print(serializer.validated_data)
         serializer.save(client=self.request.user)
 
     def get_queryset(self):
