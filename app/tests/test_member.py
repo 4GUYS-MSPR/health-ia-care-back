6cc64740829792dfbdb123138337f7e298f76b7e
namania <mael.namania@gmail.com>
2026-04-30 15:04:50 +0200

    [00] fix: test

--- app/tests/test_member.py
+++ app/tests/test_member.py
@@ -20,23 +20,23 @@ class TestMember(APITestCase): # pylint: disable=too-many-instance-attributes
         self.serializer = MemberSerializer(instance=self.member_4)
 
     def test_should_return_a_list_of_members(self):
-        self.user.force_login(self.user_1)
-        self.user.force_authenticate(user=self.user_1)
-        response = self.user.get(self.url)
+        self.client.user.force_login(self.user_1)
+        self.client.user.force_authenticate(user=self.user_1)
+        response = self.client.user.get(self.url)
         self.assertEqual(response.status_code, 200)
         print("Le code de retour est bien 200")
         self.assertIsInstance(response.data["results"], list )
         print("L'api retourne un resultat sous forme de liste")
 
     def test_should_return_unauthorized_response_if_user_is_not_authentified(self):
-        response = self.user.get(self.url)
+        response = self.client.user.get(self.url)
         self.assertEqual(response.status_code, 401)
         print("L'API retourne un code erreur 401")
 
     def test_should_return_only_member_of_logged_client(self):
-        self.user.force_login(self.user_1)
-        self.user.force_authenticate(user=self.user_1)
-        response = self.user.get(self.url)
+        self.client.user.force_login(self.user_1)
+        self.client.user.force_authenticate(user=self.user_1)
+        response = self.client.user.get(self.url)
         self.assertEqual(len(response.data['results']),2)
         print("L'api retourne seulement les membres d'un user donné")
 
