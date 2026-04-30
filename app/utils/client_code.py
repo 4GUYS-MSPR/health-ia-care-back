df690c08f6f4f84b5fff9adbc4b848f91374f0ab
namania <mael.namania@gmail.com>
2026-04-30 15:53:33 +0200

    [00] hotfix: Simple client code

--- app/utils/client_code.py
+++ app/utils/client_code.py
@@ -1,13 +1,12 @@
 import random
 import string
 
-from app.models.client import Client
-
 def gen_code():
     chars = string.ascii_uppercase + string.digits
     return ''.join(random.choice(chars) for _ in range(6))
 
 def gen_client_code():
+    from app.models.client import Client
     while True:
         code = gen_code()
         if not Client.objects.filter(code=code).exists():
