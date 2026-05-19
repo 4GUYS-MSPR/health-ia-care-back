0694ebc9835a807bd4d0da92cc03a82b9217e73a
namania <mael.namania@gmail.com>
2026-05-19 13:51:10 +0200

    [00] hotfix: increase token time

--- healthIA/settings.py
+++ healthIA/settings.py
@@ -102,7 +102,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'AUTH_HEADER_TYPES': ('Bearer',),
     'ROTATE_REFRESH_TOKENS': True,
