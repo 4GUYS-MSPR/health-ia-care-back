226356e965fc5839c343e6d6f095b662cd09a162
namania <mael.namania@gmail.com>
2026-05-19 13:41:02 +0200

    [00] hotfix: get_query_all_for_user remove client filter to be user

--- core/utils/query.py
+++ core/utils/query.py
@@ -3,4 +3,4 @@ from django.db import models
 from core.utils.types import AnyUser
 
 def get_query_all_for_user(model: models.Model, user: AnyUser):
-    return model.objects.all() if user.is_superuser else model.objects.all().filter(client=user)
+    return model.objects.all() if user.is_superuser else model.objects.all().filter(user=user)
