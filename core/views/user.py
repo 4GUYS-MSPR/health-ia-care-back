52d4d6b27dd494de3c9b98daba0bf6465b45f043
namania <mael.namania@gmail.com>
2026-05-20 16:30:13 +0200

    [00] wip: debug

--- core/views/user.py
+++ core/views/user.py
@@ -3,7 +3,6 @@ from django.http import HttpRequest
 
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
@@ -63,11 +62,9 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
 
-    @action(detail=True, methods=['get', 'patch'], parser_classes=[MultiPartParser, FormParser])
+    @action(detail=True, methods=['get', 'patch'])
     def avatar(self, request, pk=None): # pylint: disable=unused-argument
-        print("test")
         user = self.get_object()
-        print("test2")
 
         if request.method == 'GET':
             if hasattr(user, 'avatar') and user.avatar.value:
@@ -76,8 +73,6 @@ class UserViewSet(viewsets.ModelViewSet):
 
         print("test3")
         if request.method == 'PATCH':
-            print("test4")
-            print(request.FILES)
             file = request.FILES.get('avatar')
             if not file:
                 return Response(
