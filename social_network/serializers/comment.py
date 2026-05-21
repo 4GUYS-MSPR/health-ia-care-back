f064283708f6aead6a01eed60a1fd5be1bfc5cdb
namania <mael.namania@gmail.com>
2026-05-21 23:05:23 +0200

    [00] fix: comments

--- social_network/serializers/comment.py
+++ social_network/serializers/comment.py
@@ -6,7 +6,8 @@ from social_network.models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(source="member.user", read_only=True)
+    publication_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Comment
-        fields = ["id", "content", "user", "created_at"]
+        fields = ["id", "publication_id", "content", "user", "created_at"]
